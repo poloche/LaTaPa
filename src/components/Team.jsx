43b662058819ba1a5f91d18688dc3b7b9f494aad
paolo.lizarazu <paolo.lizarazu@consensus.com>
2024-09-18 11:14:12 -0400

    Fixing header removing button and centering teams

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,7 +7,7 @@ export const Team = (props) => {
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet me</h2>
           <p>
-            some description
+            As the founder of LatAm Talent Partners, I am passionate about bridging the gap between businesses and top-tier talent in Latin America. With years of experience in strategic sales and business development, my focus is on fostering meaningful connections and building long-lasting relationships that drive success for both clients and candidates. I believe in the power of collaboration and am dedicated to helping companies scale efficiently by providing access to exceptional remote professionals, while ensuring every partnership is rooted in trust, transparency, and mutual growth.
           </p>
         </div>
         <div id="row" class="row">
